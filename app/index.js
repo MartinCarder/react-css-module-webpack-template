d207067e8abc5fb354b483bdb14aa0abc9bf4e60
Martin Carder <mcarder@syzygy.co.uk>
2017-03-26 20:31:33 +0100

    Finish off dev build

---
+++ app/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+require('./styles/main.scss');
+
+render(
+  <p>Hello World!</p>,
+  document.getElementById('app'),
+);
---
+++ libs/webpack/index.js
@@ -0,0 +1,5 @@
+module.exports = {
+  fontLoader: require('./fontLoader').default,
+  imageLoader: require('./imageLoader').default,
+  scssLoader: require('./scssLoader').default,
+};
